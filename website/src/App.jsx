b28d4f82a520d9056a44ac82605725c543d0eead
Maya Ramkishun <mayaramkishuncsh2027@gmail.com>
2024-11-07 10:22:43 -0500

    Delteted react-bootstrap and reinstalled og bootstrap

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import Home from './pages/Home';
 
 function App() {
