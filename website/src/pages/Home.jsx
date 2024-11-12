9d4e632421afbfd30709949d63677941c9e9233c
mayayr115 <mayayr115@gmail.com>
2024-11-12 15:54:34 -0500

    Added Navbar and added flex

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../components/Navbar'
+
 function Home() {
   return (
     <>
+    <div className="d-flex justify-content-start">
       <h1>Maya Ramkishun's Website</h1>
+    </div>
+      <Navbar></Navbar>
       <p>This is Maya.</p>
     </>
   )
