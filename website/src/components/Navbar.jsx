85cbefaf50e3c1e478de0044abad79251da38400
mayayr115 <mayayr115@gmail.com>
2024-11-13 14:18:04 -0500

    Added Microlink and displayed projects

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -24,39 +24,18 @@ function Navbar() {
                     </li>
                     <li className="nav-item">
                     <a className="nav-link" href="#">
-                        Link
+                        Projects
                     </a>
                     </li>
-                    <li className="nav-item dropdown">
-                    <a
-                        className="nav-link dropdown-toggle"
-                        href="#"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        Dropdown
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        Experience
                     </a>
-                    <ul className="dropdown-menu">
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Action
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Another action
-                        </a>
-                        </li>
-                        <li>
-                        <hr className="dropdown-divider" />
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Something else here
-                        </a>
-                        </li>
-                    </ul>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        Contact
+                    </a>
                     </li>
                 </ul>
                 </div>
--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,14 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '../components/Navbar'
+import Microlink from '@microlink/react';
+import styled from 'styled-components';
 
 function Home() {
+  // const MyCustomCard = styled(Microlink)`
+  // font-family: 'Nitti, "Microsoft YaHei", 微软雅黑, monospace';
+  // max-width: 100%;
+  // border-radius: 0.42857em;
+  // `
   return (
     <>
-    <div className="d-flex justify-content-start">
-      <h1>Maya Ramkishun's Website</h1>
-    </div>
+      <h1 className="d-flex justify-content-start">Maya Ramkishun's Website</h1>
       <Navbar></Navbar>
       <p>This is Maya.</p>
+      <h2 className="d-flex justify-content-start">My Projects</h2>
+      <div className='d-inline-flex p-2 flex-row mb-3'>
+        <Microlink className='m-2' url='https://github.com/marcy-capstone/physics-calculator' />
+        <Microlink className='m-2' url='https://github.com/Wheeze-Watchers/wheeze-watchers-civic-tech' />
+        <Microlink className='m-2' url='https://github.com/Maya-and-Moh-K/edible-adventures'/>
+        <Microlink className='m-2' url='https://github.com/Maya-and-Cris-Supershow/Mod-4-Project' />
+      </div>
     </>
   )
 }
